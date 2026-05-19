7edef1c6f228d6f50617d5ab8b1361af82b5d7db
Mouhamed <mouhamed12@192.168.1.13>
2026-05-19 14:58:02 +0000

    fix allowed hosts

--- gestion_stock/gestion_stock/settings.py
+++ gestion_stock/gestion_stock/settings.py
@@ -16,7 +16,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'gestion-stock-production-aed7.up.railway.app',
+]
 
 
 # Application definition
