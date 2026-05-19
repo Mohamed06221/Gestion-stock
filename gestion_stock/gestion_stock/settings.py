09f9a127da5de9639e4e531476624c8202ff599f
Mouhamed <mouhamed12@192.168.1.13>
2026-05-19 16:44:51 +0000

    Configuration PostgreSQL

--- gestion_stock/gestion_stock/settings.py
+++ gestion_stock/gestion_stock/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,10 +43,9 @@ ROOT_URLCONF = 'gestion_stock.urls'
 WSGI_APPLICATION = 'gestion_stock.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 LANGUAGE_CODE = 'fr'
@@ -58,4 +58,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
